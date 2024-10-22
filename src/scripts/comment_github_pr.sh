1e21f50d4934189bc50de9a04671b5425226a681
David Alberto Montaño Fetecua <david.montano@circleci.com>
2024-10-22 09:44:57 -0500

    feat: importin remote logic

---
+++ src/scripts/comment_github_pr.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+pr_response=$(gh pr list --head $CIRCLE_BRANCH --state open --json number)
+
+if [ $(echo $pr_response | jq length) -eq 0 ]; then
+  echo "No PR found to update"
+else
+  pr_number=$(echo $pr_response | jq -r ".[].number")
+  gh pr comment $pr_number --body "
+  # CircleCI App Distribution
+  |   |   |
+  |---|---|
+  | Platform | ${PLATFORM} |
+  | Name | ${APP_NAME} |
+  | ReleaseVersion | ${RELEASE_VERSION} |
+  | BuildVersion | ${BUILD_VERSION} |
+  | Identifier | ${IDENTIFIER} |
+  | Install URL | ${PAGE_URL} |
+  | QR Code | ![](https://api.qrserver.com/v1/create-qr-code/?size=100x100&data=${PAGE_URL}) |
+  "
+fi
